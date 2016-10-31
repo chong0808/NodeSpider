b83886afcfe713c0f0584848042565ecca387927
z_chong <470669925@qq.com>
2016-10-31 16:31:19 +0800

    no message

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var count = 0;
 
 
 
-configSetting("28116784")
+configSetting("22212644")
 
 // app.get('/',function(req,res){
 // 		res.send(photos);
@@ -44,55 +44,65 @@ function configSetting(questionNum) {
 				//  发送ajax间隔时间
 				ajax_timeout: 5,
 				//  下载图片速度
-				download_v: 100,
+				download_v: 50,
 				title:questionNum
 			}
 
 			//创建一个目录文件夹
-		
-							
 			fs.mkdir(__dirname + '/'+questionNum,function(err){
 				if(err){
 					console.log(err)
 				}
 				console.log('创建文件夹成功')
 			})
-				
-
 			getInitUrlList(setting);
 }
 // 获取首屏所有图片链接
 function getInitUrlList(setting) {
-
 			fetch_data_get(setting.firstLink, {})
 				.then(function(result) {
+					
 					var $ = cheerio.load(result.text);
-					var itemsAnswer = $('.zm-item-answer ');
+					var itemsAnswer = $('.zm-item-answer');
 					var maxNum = parseInt($('#zh-question-answer-num').text());
-						itemsAnswer.map(function(i, elem) {
+					
+						for (var i =0 ; i < itemsAnswer.length; i++) {
+							
+							var elem = itemsAnswer[i];
+
 							var name = $(elem).find('a.author-link').text();
-							name?name=name : name='匿名';
+						
+								name !=''? name = name : name = '匿名';
+
 							var id = $(elem).find('a.author-link').attr('href');
-							id ? id = id.substring(8,id.length) :'匿名用户';
+
+								id ? id = id.substring(8,id.length) : id = '匿名用户';
+
 							var nameSrc =  $(elem).find('a.author-link').attr('href');
-							nameSrc? nameSrc='www.zhihu.com' + nameSrc: nameSrc='zhihu.com';
-							var answerImg = $(elem).find( '.zm-item-rich-text img' );
-							var imgSrc = []
-							if(answerImg.length>0){
-								answerImg.each(function(i, el) {
-									imgSrc.push($(el).data().original);
-								})
-								photos.push({
-									'id':id,
-									'name': name,
-									'nameSrc': nameSrc,
-									'imgSrc': imgSrc
-								});
-							}else{
-								return false;
-							}
-						})
-						console.log(maxNum)
+
+								nameSrc? nameSrc='www.zhihu.com' + nameSrc: nameSrc='zhihu.com';
+
+							var answerImg = $(elem).find( '.zm-editable-content img' );
+
+							var imgSrc = [];
+								
+								if(answerImg.length>0){
+									answerImg.each(function(i, el) {
+										if($(el).data().original){
+											imgSrc.push($(el).data().original);
+										}
+									})
+									photos.push({
+										'id':id,
+										'name': name,
+										'nameSrc': nameSrc,
+										'imgSrc': imgSrc
+									});
+								}
+
+						
+						}
+						
 					getIAjaxUrlList(20, setting,maxNum);
 				})
 				.catch(function(error) {
@@ -103,45 +113,66 @@ function getInitUrlList(setting) {
 function getIAjaxUrlList(offset, setting,maxNum) {
 			fetch_data_post(setting.ajaxLink, setting.post_data_h + offset + setting.post_data_f, setting.header)
 				.then(function(result) {
-					var response = JSON.parse(result.text);
+
+					var response = JSON.parse(result.text).msg.join('');
+					// var response = JSON.parse(result.text);
 					if (offset < maxNum) {
+
 						// 把所有的数组元素拼接在一起
-						var $ = cheerio.load(response.msg.join(""));
-						var itemsAnswer = $('.zm-item-answer ');
+						var $ = cheerio.load(response);
+
+						var itemsAnswer = $('.zm-item-answer');
+						console.log('itemsAnswer----------');
+						console.log(itemsAnswer.length);
+								
 						itemsAnswer.map(function(i, elem) {
+								
 								var name = $(elem).find('a.author-link').text();
 
-								name ? name = name : name='匿名';
+
+								name.length>0 ? name = name : name='匿名';
 								
 								var id = $(elem).find('a.author-link').attr('href');
 								id ? id = id.substring(8,id.length) :id ='匿名用户';
+								
 								var nameSrc = 'www.zhihu.com' + $(elem).find('a.author-link').attr('href');
 								nameSrc? nameSrc=nameSrc: nameSrc='zhihu.com';
 								var answerImg = $(elem).find( '.zm-item-rich-text img' );
-								var imgSrc = []
+								var imgSrc = [];
+
 								if(answerImg.length>0){
+
 									answerImg.each(function(i, el) {
-										imgSrc.push($(el).attr('src'));
+
+										if($(el).attr('src')){
+
+											imgSrc.push($(el).attr('src'));
+										}
+
 									})
+
 									photos.push({
 										'id':id,
 										'name': name,
 										'nameSrc': nameSrc,
 										'imgSrc': imgSrc
 									});
+
 								}else{
 									return false;
 								}
 						})
 						setTimeout(function() {
-							offset += 20;
-							console.log("已成功抓取 " + photos.length + " 个人的答案");
+							offset += 10;
+
 							getIAjaxUrlList(offset, setting,maxNum);
+						
 						}, setting.ajax_timeout)
 					} else {
+
 						console.log("图片链接全部获取完毕，一共有" + photos.length + "个人的答案");
-						console.log(photos)
-						return downloadImg(setting);
+
+						 return downloadImg(setting);
 						// return  photos;
 					}
 				})
@@ -149,59 +180,71 @@ function getIAjaxUrlList(offset, setting,maxNum) {
 					console.log(error)
 				});
 }
-function downloadImg(asyncNum) {
-			// console.log(question)
 
-			// 有一些图片链接地址不完整没有“http:”头部,帮它们拼接完整
+
+function downloadImg(asyncNum) {
 			for(var j=0;j<photos.length;j++){
 
-				if(photos[j].name&&photos[j].nameSrc){
-					fs.mkdir(__dirname+'/'+asyncNum.title+'/'+photos[j].name+'('+photos[j].id+')',function(err){
-						if(err){console.log(err)}
-							console.log('创建目录成功')
-					})
-				}else{
-					fs.readdir(__dirname+'/'+asyncNum.title+'/'+'匿名' , function(err,files){
-						if(err){
-							console.log(err)
-						}
-						if(!files.length){
-							fs.mkdir(__dirname+'/'+asyncNum.title+'/'+'匿名',function(err){
-								if(err){console.log(err)}
-									console.log('创建匿名目录成功')
-							})
-						}
-					})
+ 				createFile(asyncNum,photos[j]);
+
+			}
+}
+
+
+function createFile(asyncNum,photos_a){
+
+	// return new Promise(function(resolve, reject ){
+
+			//这里需要用异步 promise 因为执行fs.mkdir()不能是非阻塞
+			fs.mkdir(__dirname+'/'+asyncNum.title+'/'+photos_a.name+'('+photos_a.id+')',function(err){
+
+				if(err){
+
+					console.log(err)
+
 				}
-				if(photos[j].imgSrc.length>0){
-					async.mapLimit(photos[j].imgSrc, asyncNum.download_v, function(photo, callback) {
-						var name = photos[j].name;
-						var nameId = photos[j].id;
-						fetch_data_get(photo, {})
-							.then(function(result) {
-								var fileName = path.basename(photo);
-								fs.writeFile('./'+asyncNum.title+'/'+ name+'('+nameId+')'+'/' + fileName, result.body, function(err) {
-									
-									if (err) {
-										console.log(err);
-									} else {
-										count++;
-										console.log(count + " done ");
-										callback(null, fileName);
-									}
-								})
-							})
-							.catch(function(error) {
-								console.log(error)
-							})
-					}, function(err, result) {
+
+					console.log('创建目录成功')
+					
+					loadImg(photos_a,asyncNum)
+					// resolve(photos_a);
+
+			})
+	// })
+
+}
+
+function loadImg(photo_s,asyncNum){
+	async.mapLimit(photo_s.imgSrc, asyncNum.download_v, function(photo, callback) {
+
+		console.log('load----------------------')
+			fetch_data_get(photo, {})
+				.then(function(result) {
+					var fileName = path.basename(photo);
+					
+
+					fs.writeFile('./'+asyncNum.title+'/'+ photo_s.name+'('+photo_s.id+')'+'/' + fileName, result.body, function(err) {
+						
 						if (err) {
 							console.log(err);
 						} else {
-							console.log(" all right ! ");
-							console.log(result);
+							count++;
+							console.log(count + " done ");
+							callback(null, fileName);
 						}
 					})
-				}
+				})
+				.catch(function(error) {
+					console.log(error)
+				})
+		}, function(err, result) {
+
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(" all right ! ");
+				console.log(result);
 			}
+		})
+
 }
